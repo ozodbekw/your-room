35f00f66fc6c573e4da6e2451bba3a1f0253da0c
Ozodbek Tursunaliyev <ozodbektursunaliyev11@gmail.com>
2025-01-02 10:14:34 +0500

    add SVG icons for logo and burger menu; update Tailwind CSS configuration and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["**/*.{html}"],
+  content: ["./*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      poppins: ["Poppins", "system-ui"],
+    },
   },
   plugins: [],
 };
